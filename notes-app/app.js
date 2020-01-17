50d0a99e0dbafb0f3b4fb1d8df24c0fb93e75e37
Bishal <bisalgt@gmail.com>
2020-01-17 16:44:17 +0545

    adding add functionality to our notes app

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,6 @@
 const chalk = require('chalk');
 const yargs = require('yargs');
-const getNotes = require('./notes');
+const notes = require('./notes');
 
 // Customize yargs
 yargs.version('1.1.0');
@@ -22,8 +22,7 @@ yargs.command({
         }
     },
     handler: (argv) => {
-        console.log('Title: ', argv.title);
-        console.log('Body: ', argv.body);
+        notes.addNote(argv.title, argv.body)
     }
 })
 
@@ -62,4 +61,3 @@ yargs.command({
 // also call yargs.parse()
 
 yargs.parse();
-console.log(yargs)
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,8 +1,44 @@
-
+const fs = require('fs');
+const chalk = require('chalk');
 
 function getNotes(){
     return "Your Notes is awesome. Keep on noting.";
 }
 
+function addNote(title, body){
+    const notes = loadNotes();
 
-module.exports = getNotes;
+    const duplicateNotes = notes.filter((note) => {
+        return note.title === title;
+
+    });
+
+    if (duplicateNotes.length === 0){
+        notes.push({
+            title: title,
+            body: body
+        });
+        
+        saveNotes(notes);
+        console.log(chalk.green.bgBlack.inverse.bold('Note added!'));
+    } else {
+        console.log(chalk.red.bgBlack.inverse.bold('Note title already taken!'));
+    }
+
+}
+
+const saveNotes = (notes) => {
+    const dataJSON = JSON.stringify(notes);
+    fs.writeFileSync('notes.json', dataJSON);
+}
+
+function loadNotes(){
+    try {
+        const dataBuffer = fs.readFileSync('notes.json');
+        const dataJSON = dataBuffer.toString();
+        return JSON.parse(dataJSON);
+    } catch (error) {
+        return [];
+    }
+}
+module.exports = {getNotes, addNote};
