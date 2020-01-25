8ca1d64258f8b577f95a8599b4c9c43450fcac52
Bishal <bisalgt@gmail.com>
2020-01-25 23:50:34 +0545

    forecast and forward geocoding//making the search with CLI plus dynamic

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,43 +1,31 @@
 const request = require('request')
-
 const geocode = require('./utils/geocode')
-// const url =  'https://api.darksky.net/forecast/7c8373c22d36c73dfa78cac08e353463/26,86?units=si&lang=en'
+const forecast = require('./utils/forecast')
 
-
-// request({url: url, json: true}, (error, response) => {
-
-//     if (error){
-//         console.log('Unable to connect to the network!')
-//     } else if (response.body.error) {
-//         console.log('Wrong Input. Unable to find the URL.')
-//     } else {
-//         var msg = `${response.body.daily.data[0].summary} It is currently ${response.body.currently.temperature} degrees and there is ${response.body.currently.precipProbability}% chance of rain`
-//         console.log(msg)
-//     }
+// console.log(process.argv)
+const address = process.argv[2]
+if (address) {
+    geocode(address, (error, data) => {
     
-// })
-
-// //IN LOW LEVEL ERROR I.E.NETWORK FAILURE. ERROR EXISTS AND THE RESPONSE IN UNDEFINED.
-// const geocodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/nepal.json?access_token=pk.eyJ1IjoiYmlzYWxndHQiLCJhIjoiY2s1c2c2N2FhMGFpejNrbnJ6YXR3bzdwbSJ9.r1JADDxID8y3DuTGYdvXOA&limit=1&unit=si'
-
-// request({url: geocodeUrl, json: true}, (error, response) => {
-//     if (error) {
-//         console.log('Unable to connect to location services!')
-//     } else if (response.body.features.length === 0){
-//         console.log('Cannot access the location specified. Try another location!')
-//     } else {
-//         const latitude = response.body.features[0].center[1]
-//         const longitude = response.body.features[0].center[0]
-//         console.log(`Latitude: ${latitude}`)
-//         console.log(`Longitude: ${longitude}`)
-//     }
+        if (error) {
+            return console.log(error)
+        }
+        console.log('Error : ', error)
+        console.log('Data : ', data)
     
-// })
+        forecast(data.latitude, data.longitude, (error, data) => {
+    
+            if (error) {
+                return console.log(error)
+            }
+            console.log(data)
+            const msg = `The climate for ${data.timezone} is ${data.climate}. It is ${data.temperature} degree with ${data.rainChances} % of rain.`
+            console.log(msg)
+        })
+    })
+    
+} else {
+    console.log('Supply a location in the CLI')
+}
 
 
-
-
-geocode('chitwan', (error, data) => {
-    console.log('Error : ', error)
-    console.log('Data : ', data)
-})
---
+++ weather-app/utils/forecast.js
@@ -0,0 +1,29 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+     
+    const url = 'https://api.darksky.net/forecast/7c8373c22d36c73dfa78cac08e353463/'+latitude +','+longitude+'?units=si&lang=en'
+    request({url: url, json: true}, (error, response) => {
+        if (error) {
+            // console.log('Unable to connect to location services!')
+            callback('Unable to connect to location services!', undefined)
+        } else if (response.body.error){
+            // console.log(response.body.error)
+            callback('Specified URL not found. Search Again with different value.', undefined) 
+        } else {
+            callback(undefined, {
+                timezone: response.body.timezone,
+                climate: response.body.currently.summary,
+                temperature: response.body.currently.temperature,
+                rainChances: response.body.currently.precipProbability,
+                latitude: response.body.latitude,
+                longitude: response.body.longitude
+
+            })
+        }
+    })
+
+}
+
+
+module.exports = forecast
