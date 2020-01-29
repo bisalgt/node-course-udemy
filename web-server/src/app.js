510a6fe5fcd64d4a10e69a10e4857a2ae765c189
Bishal <bisalgt@gmail.com>
2020-01-29 23:20:16 +0545

    handlebars/ dynamic page with templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,31 +5,43 @@
   const app = express()
   const publicDirectoryPath = path.join(__dirname, '../public')
 
+  app.set('view engine', 'hbs')
   app.use(express.static(publicDirectoryPath)) // To serve the directory
 
-  // app.get('', (req, res) => { // this a route
-  //     res.send('Hello express.')
-  // })
+  app.get('', (req, res) => { // this a route
+      res.render('index', {
+        name: 'Bishal Gautam',
+        age: 12
+      }) // renders file with hbs extension and renders it.
+  })
 
   console.log(__dirname)
 
   
 
-  app.get('/help', (req, res) => { // another route
-      res.send({
-          name: 'bishal',
-          age: 24
-      })
-  })
   app.get('/about', (req, res) => { // another route
-      res.send([{
-        name: 'sushil'
-      },
-      {
-        name: 'bishal'
-      }
+      res.render('about.hbs', {
+        name: 'andrew mead',
+        age: 'bishal'
+      })
+      // res.send({
+      //     name: 'bishal',
+      //     age: 24
+      // })
+  })
+  app.get('/help', (req, res) => { // another route
+    res.render('help', {
+      name: 'Helper Hemant',
+      age: 25
+    })
+    //   res.send([{
+    //     name: 'sushil'
+    //   },
+    //   {
+    //     name: 'bishal'
+    //   }
     
-    ])
+    // ])
   })
   app.get('/weather', (req, res) => { // another route
       res.send({
