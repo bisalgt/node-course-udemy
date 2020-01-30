9f06e3a3bf3d5e4bd946dc72d675b0d9d9620602
Bishal <bisalgt@gmail.com>
2020-01-30 19:20:31 +0545

    advance templating using hbs partials

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,17 +1,26 @@
   const path = require('path')
-
   const express = require('express')
-
+  const hbs = require('hbs')
+ 
   const app = express()
-  const publicDirectoryPath = path.join(__dirname, '../public')
 
+  // setting up paths
+  const publicDirectoryPath = path.join(__dirname, '../public')
+  const viewsPath = path.join(__dirname, '../templates/views')
+  const partialsPath = path.join(__dirname, '../templates/partials')
+
+  // setup handle bars engines and views location.
   app.set('view engine', 'hbs')
+  app.set('views', viewsPath)
+  hbs.registerPartials(partialsPath)
+
+  //Using the publicDirectoryPath to be used by the express app - app.
   app.use(express.static(publicDirectoryPath)) // To serve the directory
 
-  app.get('', (req, res) => { // this a route
+  app.get('', (req, res) => { // this a route 
       res.render('index', {
-        name: 'Bishal Gautam',
-        age: 12
+        title: 'Weather',
+        name: 'Bishal Gautam'
       }) // renders file with hbs extension and renders it.
   })
 
@@ -21,34 +30,27 @@
 
   app.get('/about', (req, res) => { // another route
       res.render('about.hbs', {
-        name: 'andrew mead',
-        age: 'bishal'
+        title: 'About page',
+        name: 'Bishal Gautam'
       })
-      // res.send({
-      //     name: 'bishal',
-      //     age: 24
-      // })
   })
+
+
   app.get('/help', (req, res) => { // another route
     res.render('help', {
-      name: 'Helper Hemant',
-      age: 25
+      title: 'Help page',
+      name: 'Bishal Boss'
     })
-    //   res.send([{
-    //     name: 'sushil'
-    //   },
-    //   {
-    //     name: 'bishal'
-    //   }
-    
-    // ])
   })
+
+
   app.get('/weather', (req, res) => { // another route
       res.send({
           forecast: 'It is raining',
           location: 'Nepalgunj'
       })
   })
+
 
   app.listen(3000, () => { // starting the server
       console.log('Listening on port 3000...')
